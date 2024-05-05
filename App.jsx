e5034d5af4c3d29d3df61ced70f9759a7e4bc08f
Abdul Hanan Abbasi <146124897+123superhanan@users.noreply.github.com>
2024-05-05 23:04:34 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import Head from './head/Head.jsx'
+import Main from './main/Main.jsx'
+import Foot from './Footer/Foot.jsx'
+import Skilss from './Skills/Skilss.jsx'
+import Track from './Track/Track.jsx'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Head/>
+      <Main/>
+      <Skilss/>
+      <Track/>
+      <Foot/>
+    </>
+  )
+}
+
+export default App
